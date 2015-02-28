5f74735dbe2397114c569fccc062d0d449d31b05
NikallassZadske <nikallasszadske@gmail.com>
2015-02-28 20:28:35 +0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -98,7 +98,7 @@
         website: null,
         intervalMessages: [],
         messageInterval: 5,
-        songstats: false,
+        songstats: true,
         commandLiteral: "!",
         blacklists: {
             NSFW: "",
