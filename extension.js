7a369d37c8904b9fb943a401c0b51685bbabb3a5
NikallassZadske <nikallasszadske@gmail.com>
2015-03-14 19:59:36 +0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -40,7 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("passu~ http://puu.sh/gzSQV/a9770f7bec.gif");
+                    API.sendChat("passu~ http://i.imgur.com/Ljg9jQn.png");
                 }
             }
         };
